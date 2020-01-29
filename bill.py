2af3616e7839ec553090ca4df54dbbaf54372197
malkraj03 <54039756+malkraj03@users.noreply.github.com>
2020-01-29 14:00:07 +0530

    Create bill.py

---
+++ bill.py
@@ -0,0 +1,14 @@
+def distance(c):
+    d=c*12
+    print(d)
+
+
+print("Enter the car model:")
+a=input()
+print("Enter the car number:")
+b=int(input())
+print("Twelve ruppes per kilometer")
+print("Enter the kilometer you want to travel:")
+c=int(input())
+print("your charge of travel is:")
+distance(c)
